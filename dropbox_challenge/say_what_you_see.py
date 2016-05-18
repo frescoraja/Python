24e55cc177afa319d655151d5b038a33b68d6131
David James Carter <fresco.raja@gmail.com>
2016-05-18 14:41:07 -0700

    update say what you see

--- dropbox_challenge/say_what_you_see.py
+++ dropbox_challenge/say_what_you_see.py
@@ -1,18 +1,17 @@
 def  say_what_you_see( input_strings):
     mylist = []
     for st in input_strings:
-        nums = dict()
-        for i in st:
-            if i in nums:
-                nums[i] += 1
-            else:
-                nums[i] = 1
         s = ''
-        for x in st:
-            if nums[x] != 'x':
-                s = s + str(nums[x])
-                s = s + str(x)
-                nums[x] = 'x'
+        num = st[0]
+        count = 1
+        for i in st[1:]:
+            if i == num:
+                count += 1
+            else:
+                s = s + str(count) + num
+                num = i
+                count = 1
+        s = s + str(count) + num
         mylist.append(s)
 
     return mylist
@@ -20,5 +19,9 @@ def  say_what_you_see( input_strings):
 
 res = say_what_you_see(["12","32"])
 res2 = say_what_you_see(["1232232", "1122112233443"])
+res3 = say_what_you_see(res + res2)
+res4 = say_what_you_see(res3)
 print res
 print res2
+print res3
+print res4
